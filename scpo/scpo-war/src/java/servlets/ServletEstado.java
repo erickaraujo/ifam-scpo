eb2915d1fb93d5c64c99c4d855693d02308bda15
erick.araujo <erickaraujo04@gmail.com>
2016-12-06 12:17:38 -0400

    consumindo servlet no jsp e importação de css

--- scpo/scpo-war/src/java/servlets/ServletEstado.java
+++ scpo/scpo-war/src/java/servlets/ServletEstado.java
@@ -6,74 +6,58 @@
 package servlets;
 
 import api.dto.EstadoDTO;
-import api.resources.EstadoResource;
 import java.io.IOException;
-import javax.ejb.EJB;
-import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import modeloList.EstadoList;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.codehaus.jackson.map.ObjectMapper;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  *
  * @author erick.araujo
  */
-@WebServlet(name = "estado", urlPatterns = {"/listarEstado"})
+@WebServlet(name = "estado", urlPatterns = {"/exibeEstados"})
 public class ServletEstado extends HttpServlet {
 
-//    @Inject
-//    private EstadoResource estadoResource;
-//    @Override
-//    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-//        ResteasyClient client = new ResteasyClientBuilder().build();
-//        ResteasyWebTarget resourceApi = client.target("http://localhost:8080/scpo/api/");
-//        ResteasyWebTarget estadoApi = resourceApi.path("/estado");
-//        Response response = estadoApi.request().get();
-//        
-//        System.out.println(response.getStatus());
-//        System.out.println(response.readEntity(estadoDTO.getClass()));
-//        response.close();
-//
-//        req.setAttribute("listarEstado", "hua");
-//        System.out.println("BLA " + estadoResource.listarEstados());
-//        RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
-//        dispatcher.forward(req, res);
-//    }
-    
-    @EJB
-        private EstadoDTO estadoDTO;
-    
+    private EstadoDTO estadoDTO;
+
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         String URL = "http://localhost:8080/scpo/api/estado";
-        DefaultHttpClient httpClient = new DefaultHttpClient();
-        HttpGet httpget = new HttpGet(URL);
-        HttpResponse response = httpClient.execute(httpget);
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(URL)
+                .build();
 
-        System.out.println("RESPONSE ---> " + response);
+        Response response = client.newCall(request).execute();
+        String jsonData = response.body().string();
 
-        HttpEntity entity = response.getEntity();
-        String result = EntityUtils.toString(entity);
-        
-        System.out.println("STRING RESULT ---> " + result);
+        JSONObject jObject = new JSONObject(jsonData);
+        JSONArray jArray = jObject.getJSONArray("estados");
 
-        ObjectMapper mapper = new ObjectMapper();
-        EstadoList estadoResponse = mapper.readValue(result, EstadoList.class);
-        
-        System.out.println("LISTA ---> " + estadoResponse);
+        List<EstadoDTO> lista = new ArrayList<EstadoDTO>();
 
-        req.setAttribute("listarEstado", estadoResponse);
-        RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
+        for (int i = 0; i < jArray.length(); i++) {
+            estadoDTO = new EstadoDTO();
+            JSONObject objeto = jArray.getJSONObject(i);
+            estadoDTO.setId(objeto.getInt("id"));
+            estadoDTO.setNome(objeto.getString("nome"));
+            estadoDTO.setSigla(objeto.getString("sigla"));
+            lista.add(estadoDTO);
+        }
+        System.out.println("lista ---->" + lista);
+
+        req.setAttribute("lista", lista);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("WEB-INF/views/estado/listar.jsp");
         dispatcher.forward(req, res);
     }
 
